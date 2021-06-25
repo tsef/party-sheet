6815172c0e5e4558229f3be9ac5bd708c3cf5ed6
tsef <tsefon@gmail.com>
2021-06-25 21:52:36 +0200

    Complete refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,42 @@
-import { PartySheetApp } from "./partysheet.js"
-
-
-
-//PartySheet class
-class PartySheet {
-  static openPartySheet = () => {
-    //const html = await renderTemplate("modules/party-sheet/scripts/myFormApplication.html");
-    let data = PartySheet.prepareData();
-    const template_file = "modules/party-sheet/html-templates/test_template.html";
-    //const template_data = {
-    //  str: data["str"].value,
-    //  dex: data["dex"].value,
-    //  con: data["con"].value,
-    //  int: data["int"].value,
-    //  wis: data["wis"].value,
-    //  cha: data["cha"].value
-    //};
-    
-    let temp = [];
-    data.forEach(e=>{
-      temp.push({
-          str: e["str"].value,
-          dex: e["dex"].value,
-          con: e["con"].value,
-          int: e["int"].value,
-          wis: e["wis"].value,
-          cha: e["cha"].value
+//import the formApp that holds our Party Sheet
+import { PSWindow } from "./pswindow.js"
+class PSApp {
+    //Create a button in the footer of the actors sidebar and launch the party sheet 
+    static createButton = () => {
+        //if not GM or elevated user => no button
+        if (!game.user.isGM && !Actor.canUserCreate(game.user)) {
+            return;
         }
-      )
+        
+        let psButton = document.getElementById("ps-button");
+        if (psButton != null) { //is the button already there ?
+            return;
+        }
+            
+        //get access to the actor sidebar then footer
+        const actorsPanel = document.getElementById("actors");
+        const actorFooter = actorsPanel.getElementsByClassName("directory-footer")[0];
+        if (actorFooter) {
+            //SBUtils.log("Creating Statblock Parse button.");
+            
+            psButton = document.createElement("button");
+            psButton.innerHTML = `<i id="ps-button" class="fas fa-address-card"></i>Party Sheet`;
+            psButton.onclick = ev => PSApp.openPSForm(); //On click invoke openPSForm
+  
+            const createEntityButton = actorFooter.getElementsByClassName("create-entity")[0];
+            actorFooter.insertBefore(psButton, createEntityButton);
+        }
     }
-  );
-    let template_data = {persons: temp};
-    console.log(template_data);
-    new PartySheetApp(template_data, {template: template_file}).render(true);
+
+    static openPSForm= () => {
+        let myPSWindow = new PSWindow();
+        myPSWindow.render(true);
+    }
 }
 
-  static partysheetButton = () => {
-      console.log('Coucou tu es là !');
-          if (!game.user.isGM && !Actor.canUserCreate(game.user)) {
-              return;
-          }
-
-          let partySheetButton = document.getElementById("PartySheet-button");
-          if (partySheetButton != null) {
-              return;
-          }
-
-          const actorsPanel = document.getElementById("actors");
-          const actorFooter = actorsPanel.getElementsByClassName("directory-footer")[0];
-          if (actorFooter) {
-              //SBUtils.log("Creating Statblock Parse button.");
-
-              partySheetButton = document.createElement("button");
-              partySheetButton.innerHTML = `<i id="PartySheet-button" class="fas fa-address-card"></i>Party Sheet`;
-              partySheetButton.onclick = ev => PartySheet.openPartySheet();//console.log('tu as cliké');
-
-              const createEntityButton = actorFooter.getElementsByClassName("create-entity")[0];
-              actorFooter.insertBefore(partySheetButton, createEntityButton);
-          }
-  }
-
-  static prepareData = () => {
-    let listeActeur = [];
-    game.actors.forEach(actor => {
-      if (actor.data.type === "character") {
-        listeActeur.push(actor.data.data.abilities);
-      }
-    })
-    return listeActeur;
-  }
-}
-
-//Crochet qui permet de créer et d'afficher le bouton de Party Sheet
+//Hook that let us put a button in the footer of the actors sidebar
 Hooks.on("renderSidebarTab", async (app, html) => {
     if (app.options.id == "actors") {
-        PartySheet.partysheetButton();
+        PSApp.createButton();
     }
 });
---
+++ scripts/pswindow.js
@@ -0,0 +1,64 @@
+export class PSWindow extends FormApplication {
+    //Class constructor
+    constructor() {
+        super();
+        this.actorIdList = [];
+        //this.template_data; //the data needed to build the display
+    }
+
+    //default options for the form
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+          height: 400,
+          width: 800,
+          popOut: true,
+          template: `modules/party-sheet/html-templates/ps-template.html`,
+          id: 'party-sheet-application',
+          title: 'Party Sheet',
+          dragDrop: [{callbacks: {dragstart: this._onDragStart, drop: this._onDrop}}]
+        });
+    }
+
+    //build the data needed to render the form
+    getData(options = {}) {
+        let template_data = [];
+        this.actorIdList.forEach(id => {
+            template_data.push({
+                name: game.actors.get(id).data.name,
+                str: game.actors.get(id).data.data.abilities["str"].value,
+                dex: game.actors.get(id).data.data.abilities["dex"].value,
+                con: game.actors.get(id).data.data.abilities["con"].value,
+                int: game.actors.get(id).data.data.abilities["int"].value,
+                wis: game.actors.get(id).data.data.abilities["wis"].value,
+                cha: game.actors.get(id).data.data.abilities["cha"].value
+            })
+        });
+        return template_data;
+    }
+    
+    //what happens when we drag an actor on the form
+    async _onDrop(event) {
+        //try to extract the data from the event
+        let dropData;
+        try {
+            dropData = JSON.parse(event.dataTransfer.getData('text/plain'));
+        } catch (err) {
+            return false;
+        }
+        console.log(dropData);
+
+        if (dropData["type"] !== "Actor") {
+            console.log("Error: need an Actor type");
+            return;
+        } else {
+            if (this.actorIdList.find(e => e === dropData["id"] )) {
+                console.log("Already in array");
+                return;
+            } else {
+                this.actorIdList.push(dropData["id"]);
+                this.render();
+            }
+            //console.log(this.actorIdList);
+        }
+    }
+}
